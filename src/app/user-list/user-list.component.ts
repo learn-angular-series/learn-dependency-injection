1f542a9c8457eeab49e738e4ba3d8336caeb77c6
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-02-23 10:42:13 +0800

    update

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -4,7 +4,9 @@ import { UserListService } from './service/user-list.service';
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
+  //如果你在这里配置了UserListService，会优先使用这里的配置，这样就不是单例了
+  providers: [UserListService] 
 })
 export class UserListComponent implements OnInit {
   public userList:Array<any>;
