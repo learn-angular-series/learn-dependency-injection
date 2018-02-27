3527e183fed5e7ef8ffd537b563a9d000a1b9819
damoqoingqiu@126.com <damoqoingqiu@126.com>
2018-02-27 17:01:13 +0800

    update

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, ReflectiveInjector } from '@angular/core';
 import { UserListService } from './service/user-list.service';
+import { TestService } from './service/test.service';
 
 @Component({
   selector: 'user-list',
@@ -10,6 +11,10 @@ export class UserListComponent implements OnInit {
   public userList:Array<any>;
   //这里我们没有在构造函数里面定义userListService，Angular不会自动注射
   public userListService:UserListService;
+
+  private reflectiveInjector:ReflectiveInjector;
+
+  private testService:TestService;
 
   constructor(  
     public injector:Injector
@@ -26,5 +31,15 @@ export class UserListComponent implements OnInit {
     this.userListService.getUserList().subscribe((userList:Array<any>)=>{
       this.userList=userList;
     });
+
+    //尝试自己创建注射器，然后利用注射器自己注射TestService服务实例
+    let myInjector = ReflectiveInjector.resolveAndCreate([
+      { provide: "TestService", useClass: TestService }  
+    ]);
+
+    this.testService = myInjector.get("TestService");
+
+    console.log(this.testService);
+
   }
 }
