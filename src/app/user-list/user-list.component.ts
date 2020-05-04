dcd04b1bf5dbac8938114f0d6fe5a99e5cbbdb1c
damoqiongqiu <damoqiongqiu@126.com>
2020-05-04 12:24:55 +0800

    update-component

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { UserListService } from '../user-list/service/user-list.service';
 
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
+  providers: [ UserListService ]
 })
 export class UserListComponent implements OnInit {
   public userList:Array<any>;
 
-  constructor() { 
+  constructor(public userListService:UserListService) { 
   }
 
   ngOnInit() {
